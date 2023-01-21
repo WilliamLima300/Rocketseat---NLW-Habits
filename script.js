c606ec074ab066dbfde25311f4159875d9e12e52
WilliamLima300 <williamlima300@gmail.com>
2023-01-21 17:44:53 -0300

    4 aula

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 // adicionar constante e dar o nome conforme documentação da biblioteca
-
 const form = document.querySelector("#form-habits")
 
-// adicionar variavel para inicaiar a biblioteca
-
+//adicionar variavel para inicaiar a biblioteca
 const nlwSetup = new NLWSetup(form)
 
-// objeto tem o nome do atributo HTML datan-name
-const data = {
-  run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
-  takePills: ["01-03"],
-  journal: ["01-02"],
+//variavel para botão registrar dia
+const button = document.querySelector("header button")
+
+// adicionar eventos para adicionar o dia e salvar mudanças
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+//adicionar função ao botão registrar dia
+function add() {
+  //adiciona a data atual tira os dados do ano.
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  //guarda na variavel atraves de uma função se o dia já existe
+  const dayExists = nlwSetup.dayExists(today)
+
+  //se existir dia adiciona mensagem ao usuario
+  if (dayExists) {
+    alert("Dia já incluso 🔴")
+    return
+  }
+
+  alert("Dia adicionado com sucesso ✅")
+
+  //função para adicionar o dia
+  nlwSetup.addDay(today)
+}
+//adicionar função para salvar as mudanças
+function save() {
+  window.localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
-//adicionar ao nlwSetup ops dados da constante data
+//variavel para guardar e transformar em objeto novamente usando o JSON os dados da local Storage ou um objeto vazio
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+//adicionar ao nlwSetup os dados da constante data
 nlwSetup.setData(data)
 
 //carregar dados internos e renderizar o layout
